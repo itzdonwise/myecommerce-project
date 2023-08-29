361d0fb8e7f8ce931e7f288d82ba00f10596386c
Don Amicable <itzdonamicable@gmail.com>
2023-08-29 06:23:45 -0700

    add all fills

---
+++ JS/script.js
@@ -0,0 +1,72 @@
+'use strict';
+
+// Secret Number Created!
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+console.log(secretNumber);
+
+const displayMessage = function(msg){
+    document.querySelector('.message').textContent = msg;
+};
+
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess){
+        displayMessage('No Number 💀');
+    }
+    else if(guess === secretNumber){
+        displayMessage('Correct Number 🥳');
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('body').style.backgroundColor = '#2f9306';
+        document.querySelector('.number').style.width = '30rem';
+
+
+        if(score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    }
+    else if(guess !== secretNumber){
+        if(score > 1){
+            displayMessage(guess > secretNumber ? 'Too high 📈' : 'Too low 📉');
+            score = score - 1; // score --
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            displayMessage('You lost the game 😭');
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
+
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    displayMessage('Start Guessing ...');
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
---
+++ coded.js
@@ -0,0 +1,41 @@
+//variable in javascript
+const myfirstvariable =2
+
+
+
+const myAge =2
+const myupdatedage =myAge **6
+console.log(myupdatedage)
+
+
+// arry in javascript
+
+const myfriendsAge = [ 22,33, 12,28, 13]
+const sum = myfriendsAge[0] + myfriendsAge[1] + myfriendsAge[2] + myfriendsAge[3] + myfriendsAge[4]
+
+const totalfriend = 5
+console.log('Totai friend',totalfriend)
+console.log('total sum', sum)
+const average = sum / totalfriend
+console.log( 'average friends age', average)
+
+// decrement & increment in javascript
+// example
+
+let myAg = 22
+ myAg
+
+
+// functrion
+ function changemyAge(argument,multiple){
+    
+    myAg = myAg + argument 
+
+ }
+ changemyAge(3)
+ changemyAge(5)
+
+ console.log('my age =',myAg)
+
+
+
